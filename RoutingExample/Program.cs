df13d2d74d7538f50f26b8fd0d909c8964d5a0e7
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-23 08:46:22 +0100

    GetEndPoint

--- RoutingExample/Program.cs
+++ RoutingExample/Program.cs
@@ -1,8 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.Use(async (context, next) =>
+{
+    Endpoint? endPoint = context.GetEndpoint();
+    if (endPoint != null)
+        await context.Response.WriteAsync($"Endpoint: {endPoint.DisplayName}\n");
+    await next(context);
+});
+
 // Enable routing
 app.UseRouting();
+
+app.Use(async (context, next) =>
+{
+    Endpoint? endPoint = context.GetEndpoint();
+    if (endPoint != null)
+        await context.Response.WriteAsync($"Endpoint: {endPoint.DisplayName}\n");
+    await next(context);
+});
 
 app.UseEndpoints(endpoints =>
 {
@@ -10,13 +26,13 @@ app.UseEndpoints(endpoints =>
     // Method: GET. URL: http://localhost:{port}/map1
     endpoints.MapGet("map1", async (context) =>
     {
-        await context.Response.WriteAsync("In Map 1");
+        await context.Response.WriteAsync("In Map 1\n");
     });
 
     // Method: POST. URL: http://localhost:{port}/map2
     endpoints.MapPost("map2", async (context) =>
     {
-        await context.Response.WriteAsync("In Map 2");
+        await context.Response.WriteAsync("In Map 2\n");
     });
 });
 
