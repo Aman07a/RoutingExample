837e04e350c9ef80012f4110566010931f873ded
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-23 09:27:58 +0100

    Default Parameters

--- RoutingExample/Program.cs
+++ RoutingExample/Program.cs
@@ -6,6 +6,7 @@ app.UseRouting();
 
 app.UseEndpoints(endpoints =>
 {
+    // Eg: files/sample.txt
     endpoints.Map("files/{filename}.{extension}", async context =>
     {
         string? fileName = Convert.ToString(context.Request.RouteValues["filename"]);
@@ -13,10 +14,18 @@ app.UseEndpoints(endpoints =>
         await context.Response.WriteAsync($"In files - {fileName} - {extension}\n");
     });
 
-    endpoints.Map("employee/profile/{EmployeeName}", async context =>
+    // Eg: employee/profile/john
+    endpoints.Map("employee/profile/{EmployeeName=harsha}", async context =>
     {
         string? employeeName = Convert.ToString(context.Request.RouteValues["employeeName"]);
         await context.Response.WriteAsync($"In Employee profile - {employeeName}\n");
+    });
+
+    // Eg: products/details/id
+    endpoints.Map("products/details/{id=1}", async context =>
+    {
+        int id = Convert.ToInt32(context.Request.RouteValues["id"]);
+        await context.Response.WriteAsync($"Product details - {id}\n");
     });
 });
 
