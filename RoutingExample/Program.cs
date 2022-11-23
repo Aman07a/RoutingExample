17884afe3fd509d888618b980b60cacc084e1e3f
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-23 08:31:17 +0100

    Map, MapGet, MapPost

--- RoutingExample/Program.cs
+++ RoutingExample/Program.cs
@@ -7,6 +7,21 @@ app.UseRouting();
 app.UseEndpoints(endpoints =>
 {
     // Add your end points
+    // Method: GET. URL: http://localhost:{port}/map1
+    endpoints.MapGet("map1", async (context) =>
+    {
+        await context.Response.WriteAsync("In Map 1");
+    });
+
+    // Method: POST. URL: http://localhost:{port}/map2
+    endpoints.MapPost("map2", async (context) =>
+    {
+        await context.Response.WriteAsync("In Map 2");
+    });
 });
 
+app.Run(async context =>
+{
+    await context.Response.WriteAsync($"Request received at {context.Request.Path}");
+});
 app.Run();
